7b70c48d53a04af071b05f3fa08816426a0ae5ff
Tapiwa Kundishora <tapiwakundi@gmail.com>
2022-05-16 17:02:49 -0600

    finish track screen

--- src/components/TrackItem/TrackItem.tsx
+++ src/components/TrackItem/TrackItem.tsx
@@ -12,11 +12,11 @@ export const TrackItem = ({ track, album }: Props) => {
     const albumCover = track.album ? track.album.images[2].url : album?.images[2].url
     const albumName = track.album ? track.album.name : album?.name
 
-    return <div className={styles.track_container}>
+    return <a className={styles.track_container} href={`/track/${track.id}`}>
         <img src={albumCover} className={styles.track_image} alt="album cover" />
         <div className={styles.text_container}>
             <Typography.Callout>{track.name}</Typography.Callout>
             <Typography.Callout2>{`${track.artists[0].name} • ${albumName}`}</Typography.Callout2>
         </div>
-    </div>
+    </a>
 }
--- src/pages/Track/Track.tsx
+++ src/pages/Track/Track.tsx
@@ -1,8 +1,43 @@
+import React from "react"
+import { useParams } from 'react-router-dom'
+import { Album as AlbumModel, Track as TrackModel, ChartData } from '../../types'
+import { Typography, TrackItem, FeatureAnalysis } from "../../components"
 import { Page } from "../../containers"
-
+import * as SpotifyApi from '../../api'
+import styles from './index.module.css'
+import moment from "moment"
+import { processChartData } from '../../helpers/processChartData'
 
 export const Track = () => {
+    const { id } = useParams()
+    const [track, setTrack] = React.useState<TrackModel | null>()
+    const [trackChartData, setTrackChartData] = React.useState<ChartData[]>([])
+
+    React.useEffect(() => {
+        SpotifyApi.Tracks.getTrackInfo(id!)
+            .then(res => {
+                setTrack(res.track)
+                console.log(res);
+
+                const chartData = processChartData([res.audioFeatures])
+                setTrackChartData(chartData)
+
+            })
+    }, [])
+
+    if (!track) {
+        return <div>No track found</div>
+    }
+
     return <Page>
-        <div>TRACK SCREEN</div>
+        <section className={styles.track_info_container}>
+            <img src={track.album.images[0].url} alt="album cover" className={styles.album_cover} />
+            <div className={styles.track_text_container}>
+                <Typography.LargeTitle>{track.name}</Typography.LargeTitle>
+                <Typography.Caption>{moment(track.album.release_date).format('ll')}</Typography.Caption>
+
+            </div>
+            <FeatureAnalysis chartData={trackChartData} />
+        </section>
     </Page>
 }
